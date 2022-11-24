3a5ed405b599a9ff3e144e93f768ab088582b305
Smagul.Alkey <roket.flesh@gmail.com>
2022-11-24 19:30:01 +0600

    Alkey's homework

--- stack/stack.go
+++ stack/stack.go
@@ -5,21 +5,21 @@ type Stack struct {
 }
 
 func (s *Stack) Push(v int) {
-	s.values = append([]int{v}, s.values...)
+	s.values = append(s.values, v)
 
 }
 func (s *Stack) Pop() int {
 	if len(s.values) == 0 {
 		return 0
 	}
-	v := s.values[0]
-	s.values = s.values[1:]
+	v := s.values[len(s.values)-1]
+	s.values = s.values[:len(s.values)-1]
 	return v
 }
 func (s *Stack) Values() []int {
 
 	if len(s.values) == 0 {
-		return nil
+		return s.values
 	}
 	return s.values
 }
