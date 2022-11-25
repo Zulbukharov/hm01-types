3befb8bab4080f8c5d795dc9c205c40732b6723e
heloayer <103923930+heloayer@users.noreply.github.com>
2022-11-25 18:40:50 +0600

    Update stack.go

--- stack/stack.go
+++ stack/stack.go
@@ -1,7 +1,34 @@
 package stack
 
-type Stack struct{}
+type Stack struct {
+	data []int
+}
 
-func (s *Stack) Push(v int)    {}
-func (s *Stack) Pop() int      { return 0 }
-func (s *Stack) Values() []int { return nil }
+func (s *Stack) Push(v int) {
+	s.data = append(s.data, v)
+}
+
+func (s *Stack) Pop() int {
+	if len(s.data) == 0 {
+		return 0
+	}
+
+	last := 0                      
+	for i, k := range s.data {     // goofy loop
+		if i != len(s.data)-1 {
+			continue
+		} else {
+			last = k
+		}
+	}
+
+	s.data = s.data[:len(s.data)-1]
+	return last
+}
+
+func (s *Stack) Values() []int {
+	if len(s.data) == 0 {
+		return s.data
+	}
+	return s.data
+}
