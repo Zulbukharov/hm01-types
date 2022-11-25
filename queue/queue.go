5653162cf245b52299afbb96e4518b23a7fc72ba
heloayer <103923930+heloayer@users.noreply.github.com>
2022-11-25 16:46:38 +0600

    Update queue.go

--- queue/queue.go
+++ queue/queue.go
@@ -1,7 +1,26 @@
 package queue
 
-type Queue struct{}
+type Queue struct {
+	data []int
+}
 
-func (q *Queue) Enqueue(v int) {}
-func (q *Queue) Dequeue() int  { return 0 }
-func (q Queue) Values() []int  { return nil }
+func (q *Queue) Enqueue(v int) {
+	if len(q.data) == 0 {
+		q.data = append(q.data)
+	}
+	q.data = append(q.data, v)
+}
+
+func (q *Queue) Dequeue() int {
+	if len(q.data) == 0 && len(q.data) < 0 {
+		return 0
+	}
+
+	firstelem := q.data[0]
+	q.data = q.data[1:len(q.data)]
+	return firstelem
+}
+
+func (q Queue) Values() []int {
+	return q.data
+}
